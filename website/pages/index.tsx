11625381e07db55a8d34469046af17b1013aa513
Lu(c)ky/Coder <lukasbriza@seznam.cz>
2022-09-29 18:01:05 +0200

    UI - Home

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,10 +1,51 @@
+import styles from '../src/styles/pages/Home.module.scss'
+
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import { badgeConfig } from '../src/config/badgeConfig'
+import { Badge } from '../src/components'
+import { Typography } from '@lukasbriza/lbui-lib'
+
+import background from '../src/assets/home.webp'
 
 
 const Home: NextPage = () => {
   return (
-    <section>Home
-
+    <section className={styles.home}>
+      <Image
+        src={background}
+        alt="Home image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+      <div className={styles.layer}>
+        <section className={styles.hero}>
+          <section className={styles.heroText}>
+            <Typography
+              type="h1"
+              variant={["bold"]}
+              size="small"
+            >
+              LINSTA stavební s.r.o.
+            </Typography>
+            <Typography
+              type="subtitle1"
+              size="medium"
+            >
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat.
+              Cras elementum. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui.
+            </Typography>
+          </section>
+          <nav className={styles.badgeSection}>
+            {badgeConfig.map((item, index) => {
+              return (
+                <Badge icon={item.icon} text={item.text} url={item.url} key={index} />
+              )
+            })}
+          </nav>
+        </section>
+      </div>
     </section>
   )
 }

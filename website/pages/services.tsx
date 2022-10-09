372b381534e2cde0815c702b0e87bdb0e4c0848a
Lu(c)ky/Coder <lukasbriza@seznam.cz>
2022-10-09 15:58:47 +0200

    UI - Services init

--- website/pages/services.tsx
+++ website/pages/services.tsx
@@ -1,9 +1,34 @@
+import styles from '../src/styles/pages/Services.module.scss'
+import services from '@assets/serviceHeader.webp'
+
+import { PictureHeader, ServiceCard } from '@components'
+import { Typography } from '@lukasbriza/lbui-lib'
+
 import type { NextPage } from 'next'
 
 
 const Services: NextPage = () => {
     return (
-        <section>Services</section>
+        <section className={styles.services}>
+            <PictureHeader
+                src={services}
+                alt={"Services page header"}
+                text={"Služby"}
+            />
+            <Typography type="body1" className={styles.servicesText}>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Aliquam erat volutpat. Cras elementum. Maecenas ipsum velit, consectetuer eu lobortis ut,
+                dictum at dui. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat.
+                Cras elementum. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui.
+            </Typography>
+            <section className={styles.servicesWrapper}>
+                <ServiceCard className={styles.demolition} src={'assets/demolition.webp'} text={"ZEMNÍ PRÁCE a DEMOLICE"} />
+                <ServiceCard className={styles.communications} src={'assets/buildingCommunications.webp'} text={"VÝSTAVBA KOMUNIKACÍ"} />
+                <ServiceCard className={styles.canalizations} src={'assets/sewersConstruction.webp'} text={"VÝSTAVBA VODOVODNÍCH KANALIZACÍ"} />
+                <ServiceCard className={styles.transposrtation} src={'assets/transport.webp'} text={"NÁKLADNÍ AUTODOPRAVA"} />
+                <ServiceCard className={styles.carrent} src={'assets/machineRent.webp'} text={"PRONÁJEM STAVEBÍCH STROJŮ"} />
+            </section>
+        </section>
     )
 }
 
---
+++ website/src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,51 @@
+import styles from '../../styles/modules/ServiceCard.module.scss'
+
+import { FC } from 'react'
+import { PictureCard, SquareButton, Typography, Paper } from '@lukasbriza/lbui-lib'
+
+import { ServiceCardProps, CardBodyProps } from './ServiceCard.model'
+import clsx from 'clsx'
+
+export const ServiceCard: FC<ServiceCardProps> = (props) => {
+    const { src, text, className } = props
+
+    return (
+        <PictureCard
+            src={src}
+            square={true}
+            className={clsx([styles.card, className])}
+            body={<CardBody text={text} />}
+        />
+    )
+}
+
+const CardBody: FC<CardBodyProps> = (props) => {
+    const { text } = props
+    return (
+        <div className={styles.body}>
+            <div className={styles.layer}></div>
+            <Typography
+                type="h6"
+                size="medium"
+                variant={["bold"]}
+                className={styles.text}
+            >
+                {text}
+            </Typography>
+            <SquareButton
+                modificationClass={styles.button}
+                hoverClass={styles.hovered}
+                label={"Více"}
+                onClick={() => { console.log("click") }}
+            />
+            <div className={clsx([styles.line, styles.leftTopTop])}></div>
+            <div className={clsx([styles.line, styles.leftTopLeft])}></div>
+            <div className={clsx([styles.line, styles.rightoBottomBottom])}></div>
+            <div className={clsx([styles.line, styles.rightoBottomRight])}></div>
+            <div className={clsx([styles.line, styles.top])}></div>
+            <div className={clsx([styles.line, styles.left])}></div>
+            <div className={clsx([styles.line, styles.bottom])}></div>
+            <div className={clsx([styles.line, styles.right])}></div>
+        </div>
+    )
+}

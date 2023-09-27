33276d8a7e39e5eaa9bb43ecf95389694cb60920
Lukas Briza <lukasbriza@seznam.cz>
2023-09-27 21:40:38 +0200

    new logo in public folder

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -11,11 +11,10 @@ export default function Document() {
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="keywords" content="linsta,Linsta,linsta stavební,zemní práce,demolice,příprava území,skrývka ornice,hrubé terénní úpravy,výkop inženýrských sítí,výkop základů,výkop základových pasů,výkop bazénů,výkop jímek,hutněné násypy a zásypy,třídění a recyklace materiálů,výstavba, komunikací, silnic, chodníků, polních cest, parkovišť, cyklostezek, odstavných ploch,pronájem stavebních strojů, pronájem bagrů,výstavba vodovodů,výstavba kanalizací,vodojemy,čističky odpadních vod,čov,nákladní autodoprava,nákup a doprava kameniva,odvoz a likvidace suti,odvoz a likvidace zeminy, výkopku, sypkých materiálů,Hořovice, Praha, Beroun, Plzeň, Příbram, Benešov, Rakovník, Kladno, Stochov, Praha - východ, Praha - západ, Kralovice, Rokycany" />
                 <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" href="/favicon.ico" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ec9b0a" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
             </Head>
